3d435d51debaab573be1b4bb7634a9f79a7ed511
Sam Galison <samface@gmail.com>
2015-07-28 18:01:35 -0400

    figured out relationship btw subdivisions and time, built out bars + beats

--- sequencer.ino
+++ sequencer.ino
@@ -1,5 +1,3 @@
-#define ledPin 13
-
 /*
 
 code by sam galison, summer of 2015
@@ -21,22 +19,49 @@ https://arduinodiy.wordpress.com/2012/02/28/timer-interrupts/
 timer calculator + reference:
 https://docs.google.com/spreadsheets/d/1Kq19yXfzRZ7rHQM-4t8iTiXk9fSxJ60MgJrKHpM9SC4/pubhtml?gid=956017403&single=true
 
+tempo 120, 64th notes
+3/4 time
+
+counts per subdiv: 15624
+prescale: 8 // 0	1	0
+subdivs per beat	16
+beats per bar	3
+
+"measure" is my terminology for the cycle
 
 */
 
 
 volatile int state = LOW;
 
-int previousState = LOW
-    ;
+boolean newtick = true,
+  newbeat = true,
+  newbar = true,
+  newmeasure = true;
+
+int previousState = LOW,
+  subdivisions = 64,
+  subdivsPerBeat = 16,
+  beatsPerBar = 3,
+  barsPerMeasure = 4,
+  ticks = 1,
+  beats = 1,
+  bars = 1
+  ;
+
+int ctk = 1, ptk = 0;
 
 float amplitude = 0,
-decaySpeed = 0.01;
+  decaySpeed = 0.01;
+
+int[6] LEDs = {3, 5, 7, 8, 9, 13};
 
 
 void setup()
 {
-  pinMode(ledPin, OUTPUT);
+  for (int i = 0; i < 6; i++) {
+    pinMode(LEDs[i], OUTPUT);
+  }
 
   // initialize timer1
   noInterrupts();           // disable all interrupts
@@ -46,13 +71,16 @@ void setup()
   TCCR1B |= (1 << WGM12); // turn on ctc mode
 
   /* TCNT1 = 3035;            // preload timer 65536-16MHz/256/2Hz */
-  TCCR1B |= (1 << CS12);    // set prescaler to 256
+  TCCR1B |= (0 << CS10);    // prescale register, see sheet for config
+  TCCR1B |= (1 << CS11);    // prescale register, see sheet for config
+  TCCR1B |= (0 << CS12);    // prescale register, see sheet for config
+
   /* TIMSK1 |= (1 << TOIE1);   // enable timer overflow interrupt */
   TIMSK1 |= (1 << OCIE1A); // enable CTC interrupt
 
 
 
-  OCR1A = 31249; // desired match to compare against
+  OCR1A = 15624; // desired match to compare against (counts per note subdivision)
 
   interrupts();             // enable all interrupts
 
@@ -67,19 +95,37 @@ void setup()
 /* } */
 
 ISR(TIMER1_COMPA_vect) { // compare match ISR, occurs when match happens
-  state = !state;
-  amplitude = 1;
+
+  newtick = true;
+
+  if (ticks < subdivsPerBeat) ticks ++;
+  else {
+    // new beat
+    newbeat = true;
+    if (beats < beatsPerBar) beats ++;
+    else {
+      // new bar
+      newbar = true;
+      if (bars < barsPerMeasure) bars ++;
+      else {
+         // new measure (cycle resets)
+        newmeasure = true;
+        bars = 1;
+      beats = 1;
+    }
+    ticks = 1;
+  }
   /* digitalWrite(ledPin, state); */
 }
 
 
 void loop() {
-  /* if (state != previousState) { */
-  /*   amplitude = 1; */
-  /* } */
 
-  if (amplitude >= decaySpeed) amplitude -= decaySpeed;
-  analogWrite(ledPin, amplitude*255);
+  if(newbeat) amplitude = 1;
 
-  Serial.println(state);
+
+  newtick = false;
+  newbeat = false;
+  newbar = false;
+  newmeasure = false;
 }
